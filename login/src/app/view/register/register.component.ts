c45b7c043140117f650358e68484fb3f5056d826
e <1403523789@qq.com>
2021-07-05 23:59:22 +0800

    登录调接口

--- login/src/app/view/register/register.component.ts
+++ login/src/app/view/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-register',
@@ -12,8 +13,34 @@ export class RegisterComponent implements OnInit {
     public router: Router,
     // public activatedRoute: ActivatedRoute
   ) { }
+  user: any = {
+    number: '',
+    password:''
+  }
+  result:any
   addClick(event: any) {
     this.router.navigate(['/index'],);
+  }
+  loginClick(event: any) {
+    //调取接口
+    this.judgeLogin().then(res => {
+      if (this.result.data.status === 200 && this.result.data.message) {
+      //跳转
+        this.router.navigate(['/loginSuccess'],);
+      } else {
+        //注册成功后跳转到登录界面
+        alert("登录失败")
+        this.user=''
+      }
+    })
+    // this.router.navigate(['/index'],);
+  }
+  async judgeLogin() {
+    this.result = await axios.post('http://localhost:8090/user/login', {
+      password: this.user.password,
+      phone: this.user.number,
+    })
+    console.log(this.result)
   }
 
   ngOnInit() {
